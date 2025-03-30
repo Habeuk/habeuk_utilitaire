661660b54a5c7803a53ec6ab6cb0ce0c998291fb
kouwa stephane <kksasteph888@gmail.com>
2025-03-30 19:56:19 +0200

    remove old file js

--- wbu-atomique-theme/src/js/block/square_border.js
+++ wbu-atomique-theme/src/js/block/square_border.js
@@ -1,3 +1,2 @@
 import "@stephane888/wbu-atomique/scss/drupal/modules/habeuk_utilitaire/shares.js";
-import "@stephane888/wbu-atomique/js/block/block--square_border.js";
 import "@stephane888/wbu-atomique/scss/block/block--square_border.scss";
